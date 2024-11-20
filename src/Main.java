5bd0b6087308395ac9abeaf1979e1d20983ddce9
Yotam H <yotam4h@gmail.com>
2024-11-20 22:11:24 +0200

    updated structure

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,7 @@
-import io.github.cdimascio.dotenv.*;
-
 public class Main
 {
     public static void main(String[] args)
     {
-        // Load the .env file
-        Dotenv dotenv = Dotenv.configure().load();
-
-        // Database connection details
-        String url = dotenv.get("DB_URL");
-        String user = dotenv.get("DB_USER");
-        String password = dotenv.get("DB_PASSWORD");
-
+        System.out.println("Hello, World!");
     }
 }
