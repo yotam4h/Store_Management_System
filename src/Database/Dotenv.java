04cc0413dbd3fdb8da6bb08c7c6156cd2020fa9b
Roei <roei922@gmail.com>
2024-12-05 16:02:00 +0200

    Resolved merge conflicts

---
+++ src/Database/Dotenv.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package Database;
+
+
+class Dotenv {
+
+    public String get(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+    }
+
+}
